370cb60634ecda1736843bc76d2e4fa4cb496242
ALUNO <santosmiguels>
2026-09-16 20:09:19 -0300

    {hoje}

--- Parte07-Flask/04_poltergit/app.py
+++ Parte07-Flask/04_poltergit/app.py
@@ -26,7 +26,7 @@ def commitar():
         auto.press("enter")
         auto.write("git add .")
         auto.press("enter")
-        auto.write("git commit -m '{hoje}'")
+        auto.write('git commit -m "{hoje}"')
         auto.press("enter")
         auto.write("git push")
         auto.press("enter")
