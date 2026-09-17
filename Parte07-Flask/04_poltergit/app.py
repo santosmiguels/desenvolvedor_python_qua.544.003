6574aba66fc1180bb45a50d4906f10e550a24bdc
ALUNO <santosmiguels>
2026-09-16 21:01:23 -0300

    Commit do dia 16/09/26

--- Parte07-Flask/04_poltergit/app.py
+++ Parte07-Flask/04_poltergit/app.py
@@ -31,7 +31,7 @@ def commitar():
         auto.write("git push")
         auto.press("enter")
         auto.sleep(4)
-        auto.press("exit")
+        auto.press("quit")
         auto.sleep(2)
         auto.press("enter")
     #else:
