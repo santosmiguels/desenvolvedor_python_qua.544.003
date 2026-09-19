a0fb3f3d5835ba08e77c8164b3d0cec27a9affb1
ALUNO <santosmiguels>
2026-09-18 21:34:29 -0300

    Commit do dia 18/09/26

--- Parte07-Flask/05_captura_de_texto_ocr/app.py
+++ Parte07-Flask/05_captura_de_texto_ocr/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request
+from datetime import date
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/textoExtraido")
+def extrair_texto():
+    return render_template("extracao.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
