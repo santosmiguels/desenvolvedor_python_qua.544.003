1bd293d456dd109defeadca51bf85b75d5f6b501
ALUNO <santosmiguels>
2026-09-21 21:38:40 -0300

    Commit do dia 21/09/26

--- Parte07-Flask/05_captura_de_texto_ocr/app.py
+++ Parte07-Flask/05_captura_de_texto_ocr/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from datetime import date
+import easyocr
+import io
 
 app = Flask(__name__)
 
@@ -7,9 +9,30 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/textoExtraido")
+@app.route("/textoExtraido", methods = ['POST'])
 def extrair_texto():
-    return render_template("extracao.html")
+    if 'imagem' not in request.files:
+        return'Nehuma imagem enviada', 400
+    imagem = request.files['imagem']
+    if imagem.filename == '':
+        return "Nenhuma imagem selecionada", 400
+    render = easyocr.Reader(['en', 'pt'], gpu = False)
+    result = render.readtext(imagem.read())
+    texto_extraido = ' '.join([res[1] for res in result])
+    return render_template("extracao.html", text = texto_extraido)
+
+@app.route("/exportarTexto", methods = ['POST'])
+def exportar_texto():
+    if request.method == 'POST':
+        texto = request.form.get("texto", "")
+        arquivo_buffer = io.BytesIO(texto.encode('utf-8'))
+        return send_file(
+            arquivo_buffer,
+            mimetype='text/plain',
+            as_attachment=True,
+            download_name='texto_extraido.txt'
+        )
+    return render_template("exportar_sucesso.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
